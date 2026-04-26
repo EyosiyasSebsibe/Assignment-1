e5dbd348111e289d49bf6be05a7d2bb9ad3c0243
Eyosiyas <eyosiyas7799@gmail.com>
2026-04-26 20:40:24 +0300

    first commit

---
+++ assignment1.java
@@ -0,0 +1,14 @@
+class Animal {
+    private String name;
+    private int features;
+
+    public Animal(String name, int id) {
+        this.name = name;
+        this.features = features;
+    }
+    public void display() {
+    System.out.println("Name: " + name);
+    System.out.println("Features: " + features);
+    }
+}
+
